4bafa5887a5c2b79590064090ec3dc4b1c4a11de
jvb <jvb@newtec.eu>
2009-12-08 11:58:55 +0100

    Added test to verify that we can do without a recovery mechanism; Cleanup todo list.

--- src/test/scala/be/jvb/actorpmt/MetricMonitorMetricsAvailabilityTest.scala
+++ src/test/scala/be/jvb/actorpmt/MetricMonitorMetricsAvailabilityTest.scala
@@ -3,13 +3,24 @@ package be.jvb.actorpmt
 import org.scalatest.FunSuite
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import java.util.concurrent.{TimeUnit, CountDownLatch}
 import org.joda.time.{Interval, DateTime, Duration}
+import java.util.concurrent.{TimeUnit, CountDownLatch}
+
 /**
  * @author <a href="mailto:jvb@newtec.eu">Jan Van Besien</a>
  */
 @RunWith(classOf[JUnitRunner])
 class MetricMonitorMetricsAvailabilityTest extends FunSuite {
+
+  /**
+   * Helper class which listens to a metric availabilty messages from a provider to assert things about the availability of metrics.
+   */
+  class TestProviderListener(val expectedInterval: Interval, val notificationLatch: CountDownLatch) extends ProviderListener {
+    def processMetricAvailableMessage(message: MetricAvailableMessage) = {
+      if (message.metrics.interval == expectedInterval)
+        notificationLatch.countDown
+    }
+  }
 
   test("metrics for one derived metric definition are available when its single source metrics are available") {
     val source = new MetricDefinition("source", Duration.standardMinutes(1), Nil)
@@ -30,15 +41,7 @@ class MetricMonitorMetricsAvailabilityTest extends FunSuite {
     val expectedInterval = new Interval(now, derived.granularity)
 
     // register a listener with the monitor
-    class TestProviderListener extends ProviderListener {
-      def processMetricAvailableMessage(message: MetricAvailableMessage) = {
-        if (message.metrics.interval == expectedInterval)
-          expectedMetricIntervalReceived.countDown
-        else
-          fail("received unexpected metrics [" + message + "]")
-      }
-    }
-    monitor.registerDependant(new TestProviderListener().start)
+    monitor.registerDependant(new TestProviderListener(expectedInterval, expectedMetricIntervalReceived).start)
 
     // pretend a source metric to be available
     monitor ! MetricAvailableMessage(
@@ -61,6 +64,92 @@ class MetricMonitorMetricsAvailabilityTest extends FunSuite {
 
     // now the monitor should have provided a metric
     assert(expectedMetricIntervalReceived.await(1, TimeUnit.SECONDS))
+  }
+
+  test("late arriving source metrics trigger derived metric availability without a recovery mechanism of some sort") {
+    val source = new MetricDefinition("source", Duration.standardMinutes(1), Nil)
+    val derived = new MetricDefinition("derived", Duration.standardMinutes(2), source :: Nil)
+
+    val metricsConfiguration = source :: derived :: Nil
+
+    val monitorAgent: MonitorAgent = new MonitorAgent(metricsConfiguration)
+    val monitors: MonitorRepository = monitorAgent.start
+
+    // get the single monitor that was created
+    expect(1) {monitors.allMonitors.size}
+    val monitor = monitors.allMonitors.head
+
+    val now = new DateTime(2005, 05, 05, 10, 24, 0, 0) // somewhat random
+
+    val firstExpectedInterval = new Interval(now, derived.granularity)
+    val secondExpectedInterval = new Interval(firstExpectedInterval.getEnd, derived.granularity)
+    val thirdExpectedInterval = new Interval(secondExpectedInterval.getEnd, derived.granularity)
+
+    val firstIntervalReceived = new CountDownLatch(1)
+    val secondIntervalReceived = new CountDownLatch(1)
+    val thirdIntervalReceived = new CountDownLatch(1)
+
+    // register a listener for each expected interval with the monitor
+    monitor.registerDependant(new TestProviderListener(firstExpectedInterval, firstIntervalReceived).start)
+    monitor.registerDependant(new TestProviderListener(secondExpectedInterval, secondIntervalReceived).start)
+    monitor.registerDependant(new TestProviderListener(thirdExpectedInterval, thirdIntervalReceived).start)
+
+    // pretend all source metrics to be available for FIRST monitor interval to be available
+    monitor ! MetricAvailableMessage(
+      new Metrics(
+        source,
+        Map(ManagedObjectName("sit1") -> 1.0, ManagedObjectName("sit2") -> 2.0),
+        new Interval(firstExpectedInterval.getStart, source.granularity)),
+      new DateTime)
+    monitor ! MetricAvailableMessage(
+      new Metrics(
+        source,
+        Map(ManagedObjectName("sit1") -> 1.0, ManagedObjectName("sit2") -> 2.0),
+        new Interval(firstExpectedInterval.getStart.plus(source.granularity), source.granularity)),
+      new DateTime)
+
+    // first interval should be available
+    assert(firstIntervalReceived.await(1, TimeUnit.SECONDS))
+
+    // pretend HALF OF THE source metrics to be available for SECOND monitor interval
+    monitor ! MetricAvailableMessage(
+      new Metrics(
+        source,
+        Map(ManagedObjectName("sit1") -> 1.0, ManagedObjectName("sit2") -> 2.0),
+        new Interval(secondExpectedInterval.getStart, source.granularity)),
+      new DateTime)
+
+    // second interval should NOT YET be available
+    expect(secondIntervalReceived.getCount) {1}
+
+    // pretend all source metrics to be available for THIRD monitor interval to be available
+    monitor ! MetricAvailableMessage(
+      new Metrics(
+        source,
+        Map(ManagedObjectName("sit1") -> 1.0, ManagedObjectName("sit2") -> 2.0),
+        new Interval(thirdExpectedInterval.getStart, source.granularity)),
+      new DateTime)
+    monitor ! MetricAvailableMessage(
+      new Metrics(
+        source,
+        Map(ManagedObjectName("sit1") -> 1.0, ManagedObjectName("sit2") -> 2.0),
+        new Interval(thirdExpectedInterval.getStart.plus(source.granularity), source.granularity)),
+      new DateTime)
+
+    // third interval should be available, although SECOND interval is still not available
+    assert(thirdIntervalReceived.await(1, TimeUnit.SECONDS))
+    expect(secondIntervalReceived.getCount) {1}
+
+    // now pretend remaining HALF OF THE source metrics to be available for SECOND monitor interval
+    monitor ! MetricAvailableMessage(
+      new Metrics(
+        source,
+        Map(ManagedObjectName("sit1") -> 1.0, ManagedObjectName("sit2") -> 2.0),
+        new Interval(secondExpectedInterval.getStart.plus(source.granularity), source.granularity)),
+      new DateTime)
+
+    // now the second interval should be available completely, without having to use a recovery mechanism
+    assert(secondIntervalReceived.await(1, TimeUnit.SECONDS))
   }
 
 }
